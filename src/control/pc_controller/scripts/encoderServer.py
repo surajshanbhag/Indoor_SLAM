352d6cbf8ac65e74d74da77c610c0ec54f623a87
William Burdick-Crow <weburdic@ncsu.edu>
2017-04-28 17:53:12 -0400

    improved comments in pc control

--- src/control/pc_controller/scripts/encoderServer.py
+++ src/control/pc_controller/scripts/encoderServer.py
@@ -11,11 +11,11 @@ import socket
 
 global prev_distance,distance,Odometry_pub,robot_Heading,robot_pos
 def initiliaze_Socket():
+    """create socket on designated port"""
     s=socket.socket()
     print "Socket created"
 
     port =50679
-
     s.bind(('',port))
     print "socket bound to %s" %(port)
 
@@ -24,21 +24,24 @@ def initiliaze_Socket():
     return s
 
 def publish_odom(data):
+    """publishes odometry to ROS
+        take data argument sent by encoderRun.py in format (right,left)"""
     global prev_distance,distance,Odometry_pub,robot_Heading,robot_pos
     encoder_Data=data.split(',')
-
-    if len(encoder_Data) != 2:
+    
+    #ensure both were recieved 
+    if len(encoder_Data) != 2:                                                         
         return 0
-    #each increment is 0.01319mm of travel
+    
+    #each increment is 0.0065188 of travel
     distance=[int(side)*0.0065188 for side in encoder_Data]
-    #print "distance " , distance
-    #print "prev_distance " ,prev_distance
-
+    
+    #calculate displacement
     displacement=[cur-prev for cur,prev in zip(distance,prev_distance)]
     displacement_robot=(sum(displacement))/2
     rotation_robot=(displacement[0]-displacement[1])/0.155
-    #rotation_robot/=2
 
+    #update heading 
     robot_Heading+=rotation_robot
 
     if rotation_robot == 0:
@@ -52,17 +55,22 @@ def publish_odom(data):
     #print "robot_Heading", robot_Heading
     #print "rotation_robot ", rotation_robot
     #print "displacement_robot" ,displacement_robot
+    
+    #update robot heading
     robot_pos[0]=robot_pos[0] + displacement_robot*math.cos(robot_Heading)
     robot_pos[1]=robot_pos[1] + displacement_robot*math.sin(robot_Heading)
 
     current_time=rospy.Time.now()
-
+    
+    #convert to quaternion
     odom_quat=tf.transformations.quaternion_from_euler(0.0,0.0,robot_Heading)
     tf_broadcaster=tf.TransformBroadcaster()
 
     tf_broadcaster.sendTransform((robot_pos[0],robot_pos[1],0),odom_quat,current_time
             ,"base_link","odom")
 
+
+    #Publish odometry
     odom=Odometry()
     odom.header.stamp=current_time
     odom.header.frame_id="odom"
@@ -76,8 +84,6 @@ def publish_odom(data):
 
     prev_distance=distance
     print 'right: ',encoder_Data[0],'\tleft',encoder_Data[1],'\tpos',robot_pos
-
-
 
 
 if __name__ == '__main__':
--- src/control/pc_controller/scripts/motorControlServer.py
+++ src/control/pc_controller/scripts/motorControlServer.py
@@ -3,7 +3,6 @@
 import rospy
 from std_msgs.msg import String
 from sensor_msgs.msg import Joy
-#from xbox1control.msg import velocities
 import server
 
 global mode, pub, prevbutton
@@ -21,11 +20,15 @@ offset=[0.4,.17]#left,right
 speed=0.5
 
 def joy_call(data):
+    """joystick callback function must be configured for your joystick
+        currently configured for xbox360 controller"""
     global prevbutton, mode
     global R_vel, L_vel
     global reply
     global c
 
+    #toggle between manual and auto control using A button
+    #auto control not implemented 
     if data.buttons[0]==1 and prevbutton==0:
         if mode == 1:
             mode=0
@@ -34,35 +37,23 @@ def joy_call(data):
 
     prevbutton=data.buttons[0]
 
-    #R_vel = py_joytrim((data.axes[0]+data.axes[1]))
-    #L_vel = py_joytrim((data.axes[0]-data.axes[1]))
-
     if data.axes[6] == 1:
+        #Turn left on left D-pad press
         R_vel=speed*offset[1]
         L_vel=0
     elif data.axes[6] == -1:
+        #Turn right on right D-pad press
         R_vel=0
         L_vel=speed*offset[0]
     elif data.axes[4] > 0.9:
+        #Go forward when Right Stick is forward
         R_vel=speed*offset[1]
         L_vel=speed*offset[0]
     else:
         R_vel=0
         L_vel=0
 
-    #R_vel = py_joytrim((data.axes[1]+data.axes[0]))
-    #L_vel = py_joytrim((data.axes[1]-data.axes[0]))
 
-    #print "reply :" + reply
-    #reply = ""
-    #while reply != "OK":
-    #    print "right :"+ str(R_vel) + "\t left :" +str(L_vel)
-    #    c.send(str(R_vel)+","+str(L_vel));
-    #    reply = c.recv(4096)
-    #
-    #    if not reply:
-    #        print("Broken Pipe")
-    #        break
 
 def main():
     global R_vel, L_vel
@@ -71,39 +62,35 @@ def main():
     global c
     R_vel = 0
     L_vel = 0
-    mode = auto
+    mode = manual
+    
+    #start socket
     s = server.initialize_Socket()
+    
+    #wait for connection
     c, addr = s.accept()
     print("Got connection from", addr)
     reply = ""
-
+    
+    #init ROS node
     rospy.init_node("motorValues", anonymous=True)
+    #subscribe to joystick
     rospy.Subscriber("joy", Joy, joy_call)
-
-    #pub = rospy.Publisher('MotorControlValues', velocities, queue_size=10)
 
     rate=rospy.Rate(100)
 
+    #publish motor control values on port 4096
     while not rospy.is_shutdown():
         if mode == manual:
             reply = ""
-                #    pub.publish(R_vel,L_vel)
-            #print("R_val: "+str(R_vel))
-            #print("L_vel: "+str(L_vel))
             print "right :"+ str(R_vel) + "\t left :" +str(L_vel)
             c.send(str(R_vel)+","+str(L_vel));
             reply = c.recv(4096)
 
             if not reply:
                 print("Broken Pipe")
-            #    break
-
-        else:
-            pass
-                #pub.publish()
 
         rate.sleep()
-    #rospy.spin()
 
 if __name__ == '__main__':
     try:
